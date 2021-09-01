572cbf3a7fb7d57893198c56636878ccb2bcd6b9
lexsd6 <64539809+lexsd6@users.noreply.github.com>
2021-09-01 10:47:48 +0800

    Add files via upload

--- libcfind/__init__.py
+++ libcfind/__init__.py
@@ -1,31 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 21:11:32 2021
+
+@author: lexsd6
+"""
+
+import difflib
 import subprocess
 from collections import defaultdict
 import os
 import re
 import sys
-
+from psutil._compat import FileNotFoundError
+import libc_symbols
 
 class  finder(object):
 	def __init__(self,func=None, addr=None,num=None):
 		self.symbols={}
 		self.libcbase=None
-		self.run_state=hasattr(__import__('__main__'), '__file__')
 		self.__libc_path=os.path.normpath(os.path.join(os.path.realpath(os.path.dirname(__file__)), "../libc-database/db/"))
 		self.__fun_news=defaultdict(int)
 		self.__check(func,addr)
 		self.__search(num)
 	def __wrong(self):
-		if self.run_state:
+		if hasattr(__import__('__main__'), '__file__'):
 			sys.exit(0)
-		else :
-			return None
+		
+		return None
+
 	
 	def __check(self,func,addr):
 		if  type(func)!= str:
-			print("[\033[0;31;1mx\033[0m]wrong: func name not is string !")
+			print("[\033[0;31;1mx\033[0m] wrong: func name not is string !")
 			return self.__wrong()
 		if  type(addr)!= int:
-			print("[\033[0;31;1mx\033[0m]wrong: func address not is int !")
+			print("[\033[0;31;1mx\033[0m] wrong: func address not is int !")
 			return self.__wrong()
 		self.__fun_news['func']=func
 		self.__fun_news['addr']=addr
@@ -36,10 +45,20 @@ class  finder(object):
 		self.libcbase=self.__fun_news['addr']-self.symbols[self.__fun_news['func']]
 
 	def dump(self,func):
-		if self.libcbase:	
-			funcaddr=self.libcbase+self.symbols[func]
-			return funcaddr
-		
+		if self.libcbase:
+			try:	
+				funcaddr=self.libcbase+self.symbols[func]
+				print('[\033[0;32;1m+\033[0m] %s:\033[0;32;1m%s\033[0m'%(func,hex(funcaddr)))
+				return funcaddr
+			except:
+				mean=difflib.get_close_matches(func,self.symbols,12,0.4)
+				if len(mean)==0:
+					print("[\033[0;31;1mx\033[0m] wrong:don't find '\033[0;31;1m%s\033[0m' in libc！"%(func))
+				else:
+					print("[\033[0;31;1mx\033[0m] wrong:No symbol '\033[0;31;1m%s\033[0m' found in libc！did you mean:"%(func))
+					print(mean)
+				return self.__wrong()
+					
 	def __search(self,num):
 		if self.__fun_news['addr'] and self.__fun_news['func']:
 			reconst=re.compile("^%s .*%x"%(self.__fun_news['func'],self.__fun_news['addr']&0xfff))
@@ -57,8 +76,18 @@ class  finder(object):
 	                    			libcs.append(fname)
 		
 			if len(libcs)== 0:
-				print("[\033[0;31;1mx\033[0m]wrong: No matched, Make sure you supply a valid function name or add more libc in \033[0;31;1m %s\033[0m"%(self.__libc_path))
-				return self.__wrong()
+				mean=[]
+				
+				fnamekeys=libc_symbols.default_libc_symbols
+				mean=difflib.get_close_matches(self.__fun_news['func'],fnamekeys,12,0.4)
+				if len(mean)==0:
+					print("[\033[0;31;1mx\033[0m] wrong: No matched, Make sure you supply a valid function name or add more libc in \033[0;31;1m %s\033[0m"%(self.__libc_path))
+					return self.__wrong()
+				else :
+					print("[\033[0;31;1mx\033[0m] wrong: No matched, Make sure you supply a valid function name ,did you mean:")
+					print(mean)
+					print("or add more libc in \033[0;31;1m %s\033[0m"%(self.__libc_path))
+					return self.__wrong()
 			elif len(libcs)>1:
 				
 					print("[\033[0;32;1m*\033[0m]multi libc results:")
@@ -103,14 +132,14 @@ class  finder(object):
 	def ogg(self,level=0,num=None):
 		so_path=self.__libc_path.rstrip('.symbols')+'.so'
 		if os.path.exists(so_path)==False:
-			print("[\033[0;31;1mx\033[0m]wrong:don't find .so file in \033[0;31;1m %s\033[0m"%(self.__libc_path))
+			print("[\033[0;31;1mx\033[0m] wrong:don't find .so file in \033[0;31;1m %s\033[0m"%(self.__libc_path))
 			return self.__wrong()
 		else:
 			try:
 				x=subprocess.check_output(["one_gadget","--level",str(level),so_path])
 			
 			except FileNotFoundError:
-				print('[\033[0;31;1mx\033[0m]find out one_gadget')
+				print('[\033[0;31;1mx\033[0m] wrong:find out one_gadget')
 				return self.__wrong()
 			else:
 				oggtext=x.decode().split('\n\n')
@@ -140,15 +169,15 @@ class  finder(object):
 				return oggls+self.libcbase
 
 
+
 if __name__ == "__main__":
 	
 	x=finder('write',0xf7eb4c90,num=11)
 	x.ogg(num=0)
 	x.ogg(num=11)
-	print(x.dump('read'))
+	print(x.dump('reade'))
 	print(x.libcbase)
 	#print(x.sym['read'])
 	print(x.symbols['read'])
 	#test('x')
 	#x.search()
-
